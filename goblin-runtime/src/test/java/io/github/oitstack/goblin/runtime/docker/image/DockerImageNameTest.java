29e027f4834f973babc8e4b9c956f7314ef31aa5
jobop <zw19860913@163.com>
2022-06-23 13:13:50 +0800

    修复单元测试失败

--- goblin-runtime/src/test/java/io/github/oitstack/goblin/runtime/docker/image/DockerImageNameTest.java
+++ goblin-runtime/src/test/java/io/github/oitstack/goblin/runtime/docker/image/DockerImageNameTest.java
@@ -1,21 +1,25 @@
 package io.github.oitstack.goblin.runtime.docker.image;
 
+import com.github.dockerjava.api.DockerClient;
 import io.github.oitstack.goblin.runtime.RunTimeBaseTest;
 import org.junit.Assert;
 import org.junit.Test;
+import org.mockito.Mock;
 
 /**
  * @author yangguang
- * @date 2022/5/26
  */
 public class DockerImageNameTest extends RunTimeBaseTest {
+
+    @Mock
+    DockerClient dockerClient;
 
     @Test
     public void imageNameParseTest() {
         String repository = "docker-hub/mysql";
         final String mysqlTag = "mysql:5.7";
 
-        DockerImage dockerImageName = DockerImage.parseFrom(mysqlTag);
+        DockerImage dockerImageName = DockerImage.parseFrom(mysqlTag,dockerClient);
         Assert.assertNotNull(dockerImageName);
         Assert.assertEquals(dockerImageName.getImageName().getVersion().getVersionDesc(), "5.7");
         Assert.assertEquals(dockerImageName.getImageName().getRegistry(), REGISTRY);
