5b9d9e9b0a5ce58bcc232eaf5e2f560a0aac4a9d
jobop <zw19860913@163.com>
2022-08-25 14:16:36 +0800

    update damocles to v0.0.3

--- goblin-runtime/src/main/java/io/github/oitstack/goblin/runtime/docker/client/DamoclesManager.java
+++ goblin-runtime/src/main/java/io/github/oitstack/goblin/runtime/docker/client/DamoclesManager.java
@@ -68,7 +68,7 @@ public class DamoclesManager {
      */
     private static final int EXPOSED_PORT = 8080;
 
-    private static final String DEFAULT_DAMOCLES_TAG = "oitstack/damocles:v0.0.2";
+    private static final String DEFAULT_DAMOCLES_TAG = "oitstack/damocles:v0.0.3";
     private AtomicBoolean damoclesStarted = new AtomicBoolean(false);
 
     /**
